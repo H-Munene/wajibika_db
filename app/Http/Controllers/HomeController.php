c7aa52d2a2d9c6207abfa157a737fddf6e8c775f
Hezekiah Munene <hezemunene01@gmail.com>
2024-12-08 11:34:44 +0300

    refactor: clear comments

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,7 +18,7 @@ class HomeController extends Controller
         $mda_projects_count = DB::table('mda_projects')->count();
         $asOfmda = "30 June, 2023";
         //projects count
-        $citizen_projects_count = DB::table('citizen_audited_projects')->count();
+        $citizen_projects_count = DB::table('citizen_audit_projects')->count();
         //source
         $source = "https://x.com/MoraraKebasoSnr/status/1855594379425062936?t=a1bAaiEXelSF3exM6NgkHA&s=19 and https://x.com/MoraraKebasoSnr/status/1855593880495833507?t=AE01ygz1CZdtPZ1jrEZLcg&s=19";
         //as of
